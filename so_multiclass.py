279085472b3d5f334bb8a2c1d7d8f13daf74c7d4
nicococo <nicococo23@gmail.com>
2015-02-24 14:06:50 +0100

    stable latent ridge

--- so_multiclass.py
+++ so_multiclass.py
@@ -1,6 +1,5 @@
-from cvxopt import matrix,spmatrix,sparse,uniform
+from cvxopt import matrix, normal
 import numpy as np
-import math as math
 from so_interface import SOInterface
 
 
@@ -16,53 +15,42 @@ class SOMultiClass(SOInterface):
         SOInterface.__init__(self, X, y)
         self.num_classes = classes      
 
-    #def get_hotstart_sol(self): 
-    #    print('Generate a random solution vector for hot start.')
-    #    return  0.01*random(self.get_num_dims(), 1)
+    def get_hotstart_sol(self): 
+        print('Generate a random solution vector for hot start.')
+        return 1.0*normal(self.get_num_dims(), 1)
 
     def argmax(self, sol, idx, add_prior=False, add_loss=False):
         nd = self.feats
         d = 0  # start of dimension in sol
-        val = -10**10
+        val = -10.0**10
         cls = -1 # best class
 
         for c in range(self.num_classes):
             foo = sol[d:d+nd].trans()*self.X[:,idx]
             # the argmax of the above function
             # is equal to the argmax of the quadratic function
-            # foo = + 2*foo - normPsi
+            # foo = -normSol + 2*foo - normPsi
             # since ||\Psi(x_i,z)|| = ||\phi(x_i)|| = y \forall z   
+            # and normSol is also constant
             d += nd
             if (np.single(foo)>np.single(val)):
                 val = foo
                 cls = c
 
-        psi_idx = self.get_joint_feature_map(idx,cls)
+        psi_idx = self.get_joint_feature_map(idx, cls)
         return (val, cls, psi_idx)
 
-        
-    def logsumexp(self, sol, idx, add_loss=False, opt_type='linear'):
-        nd = self.feats
-        d = 0  # start of dimension in sol
-        val = 0.0
-        for c in xrange(self.num_classes):
-            val += exp(sol[d:d+nd].trans()*self.X[:,idx])
-            d += nd
-        return log(val)
-
-
     def calc_loss(self, idx, y):
-        return self.y[idx]!=y
-
+        return self.y[idx] != y
 
     def get_joint_feature_map(self, idx, y=-1):
-        if y==-1:
-            y=self.y[idx]
+        if y == -1:
+            y = self.y[idx]
 
         nd = self.feats
         mc = self.num_classes
-        psi = matrix(0.0,(nd*mc,1))
-        psi[nd*y:nd*(y+1)] = self.X[:,idx]
+        psi = matrix(0.0, (nd*mc, 1))
+        psi[nd*y:nd*(y+1)] = self.X[:, idx]
         return psi
 
     def get_num_dims(self):
