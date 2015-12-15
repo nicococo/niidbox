cf8a02f0d983b084f20e435c50ff39b46a6fad31
nicococo <sinnl0s>
2015-12-15 12:47:43 +0100

    @feat setup.py used for pip install process and proper dir structure

--- setup.py
+++ setup.py
@@ -4,16 +4,23 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'description': 'Non-IID Ridge Regression using CRFs.',
-    'author': 'Nico Goernitz',
+    'description': 'Non-IID Ridge Regression with latent (discrete) dependencies between examples.',
     'url': 'https://github.com/nicococo/niidbox',
-    'download_url': 'https://github.com/nicococo/niidbox',
+    'author': 'Nico Goernitz',
     'author_email': 'nico.goernitz@tu-berlin.de',
     'version': '0.1',
-    'install_requires': ['cvxopt','numba'],
+    'install_requires': ['nose', 'cvxopt', 'numba'],
     'packages': ['niidbox'],
     'scripts': [],
-    'name': 'niidbox'
+    'name': 'niidbox',
+    'classifiers':['Intended Audience :: Science/Research',
+                   'Programming Language :: Python',
+                   'Topic :: Scientific/Engineering',
+                   'Operating System :: POSIX',
+                   'Operating System :: Unix',
+                   'Operating System :: MacOS',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7']
 }
 
 setup(**config)
---
+++ tests/niidbox_tests.py
@@ -0,0 +1,11 @@
+from nose.tools import *
+import niidbox
+
+def setup():
+    print "SETUP!"
+
+def teardown():
+    print "TEAR DOWN!"
+
+def test_basic():
+    print "I RAN!"
