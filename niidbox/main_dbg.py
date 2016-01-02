@fix bug in pseudolikelihood computation @feat iset mean-pseudolikelihood added + indep map for unlabeled (or single label) cluster

niidbox/main_dbg.py
@@ -27,9 +27,9 @@ def get_test_data(exms, train):
     inds = np.random.permutation(exms)
     uinds = inds[train:]
     linds = inds[:train]
-    lbpa = TCRFR_lbpa(x.T.copy(), y[linds].copy(), linds, uinds, states=2, A=A, reg_gamma=10., reg_theta=0.8, trans_sym=[1])
-    qp   = TCRFR_QP(x.T.copy(), y[linds].copy(), linds, uinds, states=2, A=A, reg_gamma=10., reg_theta=0.8, trans_sym=[1])
-    bf   = TCRFR_BF(x.T.copy(), y[linds].copy(), linds, uinds, states=2, A=A, reg_gamma=10., reg_theta=0.8, trans_sym=[1])
+    lbpa = TCRFR_lbpa(x.T.copy(), y[linds].copy(), linds,  states=2, A=A, reg_gamma=10., reg_theta=0.8, trans_sym=[1])
+    qp   = TCRFR_QP(x.T.copy(), y[linds].copy(), linds, states=2, A=A, reg_gamma=10., reg_theta=0.8, trans_sym=[1])
+    bf   = TCRFR_BF(x.T.copy(), y[linds].copy(), linds, states=2, A=A, reg_gamma=10., reg_theta=0.8, trans_sym=[1])
     return lbpa, qp, bf, x, y, z
 
 
@@ -84,8 +84,8 @@ def test_constr_speed():
     linds = inds[:train]
 
     print('Start:')
-    cluster = [ np.array(range(exms))]
-    lbpa = TCRFR_lbpa_iset(cluster, x.T, y[linds], linds, uinds, states=2, A=A, \
+    cluster = [ np.arange(exms)]
+    lbpa = TCRFR_lbpa_iset(cluster, x.T, y[linds], linds, states=2, A=A, \
                            reg_theta=0.9, reg_gamma=1., trans_sym=[1])
     lbpa.fit(use_grads=False)
     y_pred, lat_pred = lbpa.predict()
@@ -109,13 +109,16 @@ def test_lbp():
     inds = np.random.permutation(exms)
     uinds = inds[train:]
     linds = inds[:train]
+    cluster = [np.arange(exms)]
 
-    qp = TCRFR_QP(x.T.copy(), y[linds].copy(), linds, uinds, states=2, A=A, reg_gamma=10., reg_theta=0.9, trans_sym=[1])
-    lbpa = TCRFR_lbpa(x.T.copy(), y[linds].copy(), linds, uinds, states=2, A=A, reg_gamma=10., reg_theta=0.9, trans_sym=[1])
+    qp = TCRFR_QP(x.T.copy(), y[linds].copy(), linds, states=2, A=A, reg_gamma=10., reg_theta=0.9, trans_sym=[1])
+    lbpa = TCRFR_lbpa(x.T.copy(), y[linds].copy(), linds, states=2, A=A, reg_gamma=10., reg_theta=0.9, trans_sym=[1])
+    lbpa_iset = TCRFR_lbpa_iset(cluster, x.T.copy(), y[linds].copy(), linds, states=2, A=A, reg_gamma=10., reg_theta=0.9, trans_sym=[1])
     qp.fit()
     lbpa.fit()
+    lbpa_iset.fit()
 
-    # lbpa.map_inference(qp.u, lbpa.unpack_v(qp.v))
+    # lbpa_iset.map_inference(lbpa.u, lbpa.unpack_v(lbpa.v))
     lid = lbpa.map_indep(qp.u, lbpa.unpack_v(qp.v))
 
     print 'STATES ------------------------'
@@ -125,6 +128,7 @@ def test_lbp():
     print 'True   = ', z
     print 'QP     = ', qp.latent
     print 'LBPA   = ', lbpa.latent
+    print 'LBPAc  = ', lbpa_iset.latent
     print 'Indep  = ', lid
 
 

niidbox/tcrfr_lbpa_iset.py
@@ -4,11 +4,11 @@ import numpy as np
 from numba import autojit
 
 from abstract_tcrfr import AbstractTCRFR
-from tcrfr_lbpa import TCRFR_lbpa, _extern_map_lbp
+from tcrfr_lbpa import TCRFR_lbpa
 from tools import profile
 
 
-class TCRFR_lbpa_iset(AbstractTCRFR):
+class TCRFR_lbpa_iset(TCRFR_lbpa):
     """ Pairwise Conditional Random Field for transductive regression.
 
         The graphical model is assumed to split into multiple disjunct sets
@@ -16,25 +16,33 @@ class TCRFR_lbpa_iset(AbstractTCRFR):
         tremendously.
     """
 
-    MAP_ISET_FULL = 0  # map inference on the full set of variables (fallback)
-    MAP_ISET_MEAN = 1  # full map inference only on cluster with multiple labels
-                       # otherwise, map inference only on the mean of unlabeled clusters
+    MAP_ISET_FULL = 0   # map inference on the full set of variables (fallback)
+    MAP_ISET_MEAN = 1   # full map inference only on cluster with multiple labels
+                        # otherwise, map inference only on the mean of unlabeled clusters
+    MAP_ISET_INDEP = 2  # full map inference only on cluster with multiple labels
+                        # otherwise, map inference assuming independent examples within type 0,1 clusters
 
     map_iset_inference_scheme = MAP_ISET_MEAN
+
+    num_isets = -1          # number of clusters
 
     isets = None            # list of np.array indices of cluster-memberships
     iset_type = None        # 3 types (per cluster): 0-no lbld exms, 1-one lbld exm, 2-multiple lbld exms
     iset_lbl_inds = None    # for each cluster a (np.array-)list of labeled examples
-    iset_type01 = None      # indices of clusters of type 0 or 1
+    iset_edges = None       # number of edges in each cluster
+
     iset_type1 = None       # indices of clusters of type 1
     iset_type1_lbl = None   # indices of the label of clusters of type 1
-    data_means = None       # the overall mean of the input instances for each cluster
 
+    data_means = None       # the overall mean of the input instances for each cluster
+    data_iset = None        # for each data point its corresponding cluster
 
     def __init__(self, cluster, data, labels, label_inds, states, A,
                  reg_theta=0.5, reg_lambda=0.001, reg_gamma=1.0, trans_regs=[1.0, 1.0],
                  trans_sym=[1], lbl_weight=1.0, verbosity_level=1):
+
         self.isets = cluster
+        self.num_isets = len(cluster)
         AbstractTCRFR.__init__(self, data, labels, label_inds, states, A, \
                  reg_theta, reg_lambda, reg_gamma, trans_regs, trans_sym, verbosity_level=verbosity_level)
 
@@ -43,27 +51,36 @@ class TCRFR_lbpa_iset(AbstractTCRFR):
         # calculate the means of the clusters
         type1_inds = []
         type1_lbl_inds = []
-        type01_inds = []
         self.iset_lbl_inds = []
-        self.iset_type = np.zeros(len(self.isets), dtype=np.int8)
-        self.data_means = np.zeros( (self.feats, len(self.isets)), dtype=np.float64)
-        for i in range(len(self.isets)):
+        self.iset_type = np.zeros(self.num_isets, dtype=np.int8)
+        self.data_means = np.zeros( (self.feats, self.num_isets), dtype=np.float64)
+        self.data_iset = np.zeros(self.samples, dtype=np.int32)
+
+        for i in range(self.num_isets):
+            # calculate cluster means
             self.data_means[:, i] = np.mean(self.data[:, self.isets[i]], axis=1)
             inds = np.intersect1d(self.label_inds, self.isets[i])
             self.iset_lbl_inds.append(inds)
+            self.data_iset[self.isets[i]] = i
             if inds.size == 1:
                 self.iset_type[i] = 1
                 type1_inds.append(i)
-                type01_inds.append(i)
                 ind = np.where(self.label_inds == inds[0])[0]
                 type1_lbl_inds.append(ind[0])
             elif inds.size > 1:
                 self.iset_type[i] = 2
-            else:
-                type01_inds.append(i)
-        self.iset_type01 = np.array(type01_inds, dtype=np.int)
+
         self.iset_type1 = np.array(type1_inds, dtype=np.int)
         self.iset_type1_lbl = np.array(type1_lbl_inds, dtype=np.int)
+
+        cnt_wrong_edges = 0
+        self.iset_edges = np.zeros(len(self.isets), dtype=np.int32)
+        for e in self.E:
+            self.iset_edges[self.data_iset[e[0]]] += 2
+            if self.data_iset[e[0]]!=self.data_iset[e[1]]:
+                cnt_wrong_edges += 1
+
+        print cnt_wrong_edges
 
     def print_stats(self):
         AbstractTCRFR.print_stats(self)
@@ -90,7 +107,7 @@ class TCRFR_lbpa_iset(AbstractTCRFR):
         # if full inference is requested, then just
         # let the base-class do the job.
         if self.map_iset_inference_scheme == self.MAP_ISET_FULL:
-            return TCRFR_lbpa.map_inference(self, u, v)
+            return super(TCRFR_lbpa_iset, self).map_inference(u, v)
 
         if self.latent is not None:
             self.latent_prev = self.latent
@@ -98,26 +115,64 @@ class TCRFR_lbpa_iset(AbstractTCRFR):
         # CASE 0 & 1: no or one lbld example in this cluster
         iu = u.reshape((self.feats, self.S), order='F')
         iv = v[self.trans_d_full*self.trans_n:].reshape((self.feats, self.S), order='F')
-        map_objs = np.zeros((self.S, len(self.isets)))
-        for s in range(self.S):
-            map_objs[s, :] = (1.0 - self.reg_theta)*iv[:, s].dot(self.data_means)
-            f_squares = self.labels[self.iset_type1_lbl] - iu[:, s].dot(self.data_means[:, self.iset_type1])
-            map_objs[s, self.iset_type1] -= self.reg_theta/2. * f_squares*f_squares
-        # to expand
-        iset_latent = np.argmax(map_objs, axis=0)
 
-        for i in range(len(self.isets)):
-            # CASE 2: multiple lbld examples in this cluster
+        if self.map_iset_inference_scheme == self.MAP_ISET_MEAN:
+            map_objs = np.zeros((self.S, len(self.isets)))
+            for s in range(self.S):
+                map_objs[s, :] = (1.0 - self.reg_theta)*iv[:, s].dot(self.data_means)
+                f_squares = self.labels[self.iset_type1_lbl] - iu[:, s].dot(self.data_means[:, self.iset_type1])
+                map_objs[s, self.iset_type1] -= self.reg_theta/2. * f_squares*f_squares
+            # to expand
+            latent_means = np.argmax(map_objs, axis=0)
+
+        if self.map_iset_inference_scheme == self.MAP_ISET_INDEP:
+            map_objs = np.zeros((self.S, self.samples))
+            for s in range(self.S):
+                map_objs[s, :] = (1.0 - self.reg_theta)*v[:, s].dot(self.data)
+                f_squares = self.labels - u[:, s].dot(self.data[:, self.label_inds])
+                map_objs[s, self.label_inds] -= self.reg_theta/2. * f_squares*f_squares
+                self.latent = np.argmax(map_objs, axis=0)
+
+
+        for i in range(self.num_isets):
             if self.iset_type[i] == 2:
-                lats = _extern_map_partial_lbp(self.data, self.labels, self.iset_lbl_inds[i], \
+                # CASE 2: multiple lbld examples in this cluster
+                lats = _extern_map_partial_lbp(self.data, self.labels, self.label_inds, \
                               self.N, self.N_inv, self.N_weights, u, v, self.reg_theta, self.feats, \
                               self.isets[i], self.S, self.trans_d_full, self.trans_n, \
                               self.trans_mtx2vec_full, False, self.verbosity_level)
                 self.latent[self.isets[i]] = lats[self.isets[i]]
             elif self.iset_type[i] == 0 or self.iset_type[i] == 1:
-                self.latent[self.isets[i]] = iset_latent[i]
+                if self.map_iset_inference_scheme == self.MAP_ISET_MEAN:
+                    self.latent[self.isets[i]] = latent_means[i]
 
         return self.get_joint_feature_maps()
+
+    @profile
+    def log_partition_pl(self, v):
+        # self.N is a (Nodes x max_connection_count) Matrix containing the indices for each neighbor
+        # of each node (indices are 0 for non-neighbors, therefore N_weights is need to multiply this
+        # unvalid value with 0.
+        v_em = v[self.trans_n*self.trans_d_full:].reshape((self.feats, self.S), order='F')
+        f_inner = np.zeros((self.S, self.num_isets))
+
+        for s1 in range(self.S):
+            f_trans = np.zeros(self.num_isets)
+            for s2 in range(self.S):
+                f_trans += v[self.trans_mtx2vec_full[s1, s2]]*self.iset_edges
+            f_inner[s1, :] = v_em[:, s1].dot(self.data_means) + f_trans
+
+        # exp-trick (to prevent NAN because of large numbers): log[sum_i exp(x_i-a)]+a = log[sum_i exp(x_i)]
+        max_score = np.max(f_inner)
+        f_inner = np.sum(np.exp(f_inner - max_score))
+        foo = np.log(f_inner) + max_score
+        # max_score = np.max(f_inner, axis=0).reshape((1, self.num_isets))  # max-score for each sample
+        # max_score = np.repeat(max_score, self.S, axis=0)
+        # f_inner = np.sum(np.exp(f_inner - max_score), axis=0)
+        # foo = np.sum(np.log(f_inner) + max_score)
+        if np.isnan(foo) or np.isinf(foo):
+            print('TCRFR Pairwise Potential Model: the log_partition is NAN or INF!!')
+        return foo
 
 
 @autojit(nopython=True)
@@ -167,7 +222,7 @@ def _extern_map_partial_lbp(data, labels, label_inds, N, N_inv, N_weights, \
 
                         sum_msg = 0.
                         for n1 in range(num_neighs):
-                            if not n1==j:
+                            if not n1 == j:
                                 sum_msg += msgs[neighs[n1], N_inv[i, n1], t]
 
                         foo[t] = unary[t, i] + (1.0 - theta)*(v[trans_mtx2vec_full[s, t]] + sum_msg)
@@ -197,7 +252,7 @@ def _extern_map_partial_lbp(data, labels, label_inds, N, N_inv, N_weights, \
         foo[t] = unary[t, i] + (1.0 - theta)*sum_msg
 
     # backtracking step
-    idxs = np.zeros(samples, dtype=np.int32)
+    idxs = np.zeros(sample_inds.size, dtype=np.int32)
     latent[i] = np.argmax(foo)
     idxs[0] = i
     cnt = 1
@@ -208,5 +263,4 @@ def _extern_map_partial_lbp(data, labels, label_inds, N, N_inv, N_weights, \
                 latent[N[i, j]] = psis[N[i, j], N_inv[i, j], latent[i]]
                 idxs[cnt] = N[i, j]
                 cnt += 1
-
     return latent